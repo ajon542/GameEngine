3e2e972139e1fb896b457d69e889a4d1b05add8d
Andrew Jones <ajon542@gmail.com>
2015-12-08 09:03:34 -0800

    Setup basic structure for MVVM

---
+++ GameEngine/ViewModel/DelegateCommand.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace GameEngine.ViewModel
+{
+    /// <summary>
+    /// Implementation of the ICommand interface.
+    /// </summary>
+    public class DelegateCommand : ICommand
+    {
+        /// <summary>
+        /// The action to execute.
+        /// </summary>
+        private Action execute;
+
+        /// <summary>
+        /// The predicate for determining whether to execute the command.
+        /// </summary>
+        private Predicate<object> canExecute;
+
+        /// <summary>
+        /// Internal event handler.
+        /// </summary>
+        private event EventHandler CanExecuteChangedInternal;
+
+        /// <summary>
+        /// CanExecuteChanged event handler.
+        /// </summary>
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+                this.CanExecuteChangedInternal += value;
+            }
+
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+                this.CanExecuteChangedInternal -= value;
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DelegateCommand"/> class.
+        /// </summary>
+        /// <param name="execute">The action to execute.</param>
+        public DelegateCommand(Action execute)
+            : this(execute, DefaultCanExecute)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DelegateCommand"/> class.
+        /// </summary>
+        /// <param name="execute">The action to execute.</param>
+        /// <param name="canExecute">The predicate for determining whether to execute the command.</param>
+        public DelegateCommand(Action execute, Predicate<object> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException("execute");
+            }
+
+            if (canExecute == null)
+            {
+                throw new ArgumentNullException("canExecute");
+            }
+
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        /// <summary>
+        /// Determine if we can execute the command based on the predicate.
+        /// </summary>
+        /// <param name="parameter">The parameter to pass.</param>
+        /// <returns>True if the commmand can be executed; false otherwise.</returns>
+        public bool CanExecute(object parameter)
+        {
+            return canExecute != null && canExecute(parameter);
+        }
+
+        /// <summary>
+        /// Execute the command.
+        /// </summary>
+        /// <param name="parameter">The parameter to pass.</param>
+        public void Execute(object parameter)
+        {
+            // TODO: Optionally pass parameter if we use Action<object> instead of just Action.
+            execute();
+        }
+
+        /// <summary>
+        /// Invoke the event handler.
+        /// </summary>
+        public void OnCanExecuteChanged()
+        {
+            EventHandler handler = CanExecuteChangedInternal;
+            if (handler != null)
+            {
+                //DispatcherHelper.BeginInvokeOnUIThread(() => handler.Invoke(this, EventArgs.Empty));
+                handler.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        /// <summary>
+        /// Execute the command always by default.
+        /// </summary>
+        /// <param name="parameter">The parameter to pass.</param>
+        /// <returns>True.</returns>
+        private static bool DefaultCanExecute(object parameter)
+        {
+            return true;
+        }
+    }
+}
