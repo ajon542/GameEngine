4d34f56829ddb99a329b7e78694cf6aa5ad4ab29
Andrew Jones <ajon542@gmail.com>
2015-12-08 20:07:31 -0800

    Interesting problem
    So we bound the rgb values to their view-model properties. However, the
    code behind is still using the view.Value rather than being notified of
    the update in the viewModel. This is a weird situation because the the
    Paint method being called from OpenTk. Need to think about this some
    more.

--- GameEngine/ViewModel/SceneViewModel.cs
+++ GameEngine/ViewModel/SceneViewModel.cs
@@ -8,5 +8,47 @@ namespace GameEngine.ViewModel
 {
     public class SceneViewModel : ViewModelBase
     {
+        private double rValue;
+        private double gValue;
+        private double bValue;
+
+        public double RValue
+        {
+            get 
+            {
+                return rValue; 
+            }
+            set
+            {
+                rValue = value;
+                OnPropertyChanged("RValue");
+            }
+        }
+
+        public double GValue
+        {
+            get
+            {
+                return gValue;
+            }
+            set
+            {
+                gValue = value;
+                OnPropertyChanged("GValue");
+            }
+        }
+
+        public double BValue
+        {
+            get
+            {
+                return bValue;
+            }
+            set
+            {
+                bValue = value;
+                OnPropertyChanged("BValue");
+            }
+        }
     }
 }
