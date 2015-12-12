f3f92dac41b0e7baed410cb1003b7bf9df7dadd4
Andrew Jones <ajon542@gmail.com>
2015-12-12 12:30:58 -0800

    Moved the opengl stuff into its own control
    When the xaml file containing the OpenGLControl is open and the project
    is cleaned, the custom user control cannot be displayed. I think if the
    OpenGLControl was moved into its own library (so it was not part of the
    clean) this issue would not occur.

---
+++ GameEngine/View/OpenGLControl.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Forms.Integration;
+using System.Windows.Threading;
+
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+namespace GameEngine.View
+{
+    /// <summary>
+    /// Interaction logic for OpenGLControl.xaml
+    /// </summary>
+    public partial class OpenGLControl : System.Windows.Controls.UserControl
+    {
+        int vbo;
+
+        private GLControl glControl;
+
+        private DateTime lastMeasureTime;
+
+        public OpenGLControl()
+        {
+            InitializeComponent();
+
+            if(DesignerProperties.GetIsInDesignMode(this))
+            {
+                return;
+            }
+
+            // Start a timer for every millisecond.
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(1);
+            timer.Tick += TimerOnTick;
+            timer.Start();
+
+            Core.Timer.Instance.Init();
+        }
+
+        private void CreateVertexBuffer()
+        {
+            Vector3[] vertices = new Vector3[3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                float xpos = i / 5f;
+                vertices[i] = new Vector3(xpos, 0, -1);
+            }
+
+            GL.GenBuffers(1, out vbo);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
+            GL.BufferData<Vector3>(BufferTarget.ArrayBuffer,
+                                   new IntPtr(vertices.Length * Vector3.SizeInBytes),
+                                   vertices, BufferUsageHint.StaticDraw);
+        }
+
+        private void WindowsFormsHost_Initialized(object sender, EventArgs e)
+        {
+            if (DesignerProperties.GetIsInDesignMode(this))
+            {
+                return;
+            }
+
+            var flags = GraphicsContextFlags.Default;
+
+            glControl = new GLControl(new GraphicsMode(32, 24), 2, 0, flags);
+            glControl.MakeCurrent();
+            glControl.Paint += Paint;
+            glControl.Dock = DockStyle.Fill;
+            (sender as WindowsFormsHost).Child = glControl;
+            SetupViewport();
+
+            CreateVertexBuffer();
+        }
+
+        private void SetupViewport()
+        {
+            GL.Viewport(0, 0, glControl.Width, glControl.Height);
+
+            double aspectRatio = glControl.Width / (double)glControl.Height;
+            float fov = 1.0f;
+            float near = 1.0f;
+            float far = 1000.0f;
+
+            Matrix4 perspectiveMatrix =
+               Matrix4.CreatePerspectiveFieldOfView(fov, (float)aspectRatio, near, far);
+
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref perspectiveMatrix);
+        }
+
+        private void Paint(object sender, PaintEventArgs e)
+        {
+            // Update the timer instance.
+            Core.Timer.Instance.Update();
+
+            GL.ClearColor(Color.Red);
+            GL.Clear(
+                ClearBufferMask.ColorBufferBit |
+                ClearBufferMask.DepthBufferBit |
+                ClearBufferMask.StencilBufferBit);
+
+            GL.Color3(Color.Yellow);
+            GL.PointSize(5);
+            GL.EnableVertexAttribArray(0);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 0, 0);
+            GL.DrawArrays(PrimitiveType.Points, 0, 3);
+            GL.DisableVertexAttribArray(0);
+
+            glControl.SwapBuffers();
+        }
+
+        private void Resized(object sender, RoutedEventArgs e)
+        {
+            SetupViewport();
+        }
+
+        private void TimerOnTick(object sender, EventArgs e)
+        {
+            if (DateTime.Now.Subtract(lastMeasureTime) > TimeSpan.FromSeconds(1))
+            {
+                FpsCounter.Content = Core.Timer.Instance.Fps;
+                lastMeasureTime = DateTime.Now;
+            }
+            glControl.Invalidate();
+        }
+    }
+}
--- GameEngine/View/SceneView.xaml.cs
+++ GameEngine/View/SceneView.xaml.cs
@@ -16,120 +16,13 @@ namespace GameEngine.View
     /// </summary>
     public partial class SceneView : System.Windows.Controls.UserControl
     {
-        int vbo;
-
-        private GLControl glControl;
-
-        private DateTime lastMeasureTime;
-
         public SceneView()
         {
             InitializeComponent();
-
-            // Start a timer for every millisecond.
-            DispatcherTimer timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromMilliseconds(1);
-            timer.Tick += TimerOnTick;
-            timer.Start();
-
-            Core.Timer.Instance.Init();
-        }
-
-        private void CreateVertexBuffer()
-        {
-            Vector3[] vertices = new Vector3[3];
-
-            for (int i = 0; i < 3; i++)
-            {
-                float xpos = i / 5f;
-                vertices[i] = new Vector3(xpos, 0, -1);
-            }
-
-            GL.GenBuffers(1, out vbo);
-            GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
-            GL.BufferData<Vector3>(BufferTarget.ArrayBuffer,
-                                   new IntPtr(vertices.Length * Vector3.SizeInBytes),
-                                   vertices, BufferUsageHint.StaticDraw);
-        }
-
-        private void WindowsFormsHost_Initialized(object sender, EventArgs e)
-        {
-            var flags = GraphicsContextFlags.Default;
-
-            glControl = new GLControl(new GraphicsMode(32, 24), 2, 0, flags);
-            glControl.MakeCurrent();
-            glControl.Paint += Paint;
-            glControl.Dock = DockStyle.Fill;
-            (sender as WindowsFormsHost).Child = glControl;
-            SetupViewport();
-
-            CreateVertexBuffer();
-        }
-
-        private void SetupViewport()
-        {
-            GL.Viewport(0, 0, glControl.Width, glControl.Height);
-
-            double aspectRatio = glControl.Width / (double)glControl.Height;
-            float fov = 1.0f;
-            float near = 1.0f;
-            float far = 1000.0f;
-
-            Matrix4 perspectiveMatrix =
-               Matrix4.CreatePerspectiveFieldOfView(fov, (float)aspectRatio, near, far);
-
-            GL.MatrixMode(MatrixMode.Projection);
-            GL.LoadMatrix(ref perspectiveMatrix);
-        }
-
-        private void Paint(object sender, PaintEventArgs e)
-        {
-            if (DrawFPS)
-            {
-            }
-
-            // Update the timer instance.
-            Core.Timer.Instance.Update();
-
-            GL.ClearColor(
-                (float)Red.Value,
-                (float)Green.Value,
-                (float)Blue.Value,
-                1);
-            GL.Clear(
-                ClearBufferMask.ColorBufferBit |
-                ClearBufferMask.DepthBufferBit |
-                ClearBufferMask.StencilBufferBit);
-
-            GL.Color3(Color.Yellow);
-            GL.PointSize(5);
-            GL.EnableVertexAttribArray(0);
-            GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
-            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 0, 0);
-            GL.DrawArrays(PrimitiveType.Points, 0, 3);
-            GL.DisableVertexAttribArray(0);
-
-            glControl.SwapBuffers();
-        }
-
-        private void Resized(object sender, RoutedEventArgs e)
-        {
-            SetupViewport();
         }
 
         private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            glControl.Invalidate();
-        }
-
-        private void TimerOnTick(object sender, EventArgs e)
-        {
-            if (DateTime.Now.Subtract(lastMeasureTime) > TimeSpan.FromSeconds(1))
-            {
-                FpsCounter.Content = Core.Timer.Instance.Fps;
-                lastMeasureTime = DateTime.Now;
-            }
-            glControl.Invalidate();
         }
 
         #region Example DependencyProperties
