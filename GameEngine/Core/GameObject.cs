c00bdf7ea05330cab703dad2b9d0f586e4260473
Andrew Jones <ajon542@gmail.com>
2015-12-14 00:05:34 -0800

    notes

--- GameEngine/Core/GameObject.cs
+++ GameEngine/Core/GameObject.cs
@@ -20,6 +20,15 @@ namespace GameEngine.Core
         // A GameObject can be attached to other GameObjects and be influenced by the
         // behaviour of their parent.
 
+        // One thing I'm going to have to solve is the view-model to view scene data
+        // transfer. I say data transfer, but it is really just a way of notifying the
+        // view of the information it needs to display the scene.
+        // The scene is not going to be doing much apart from setting the stage for the
+        // game. The game is really going to be doing all the work and is to be handling
+        // the object interaction.
+        // The scene should be able to be described with a simple data format. It is the
+        // scripts attached to the objects that will determine their behaviour in the scene.
+
         // Use case:
         // Add an empty GameObject to the hierarchy.
         // Add different components to the GameObjects.
