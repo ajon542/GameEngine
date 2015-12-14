5aa745f4f996493696c955da5d61e9e9cc027039
Andrew Jones <ajon542@gmail.com>
2015-12-13 23:38:45 -0800

    Adding the king of all base classes

---
+++ GameEngine/Core/GameObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine.Core
+{
+    public class GameObject
+    {
+        // This is intended to be the base class of all objects that can
+        // exist in the scene.
+        // At minimum, a GameObject is going to have a position, scale and rotation.
+        // A GameObject might have a mesh, renderer, shader, input controllers etc.
+    }
+}
