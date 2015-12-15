4763b14055ff9c3bbece1fe3497f58eb6b2be43c
Andrew Jones <ajon542@gmail.com>
2015-12-14 19:58:23 -0800

    Created a unit test project

--- GameEngine/ViewModel/SceneViewModel.cs
+++ GameEngine/ViewModel/SceneViewModel.cs
@@ -44,26 +44,6 @@ namespace GameEngine.ViewModel
         public SceneViewModel()
         {
             SceneList = new List<Scene> { new SceneA() };
-
-            GameObject root = new GameObject("Root");
-            GameObject c1 = new GameObject("C1");
-            GameObject c2 = new GameObject("C2");
-            GameObject c3 = new GameObject("C3");
-
-            root.AddChild(c1);
-            root.AddChild(c2);
-            root.AddChild(c3);
-
-            string output = JsonConvert.SerializeObject(root, Formatting.Indented, new JsonSerializerSettings
-            {
-                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
-            });
-            // This almost deserializes correctly. We would just need to traverse the tree
-            // and set all the "parent" references.
-            GameObject loadedRoot = JsonConvert.DeserializeObject<GameObject>(output, new JsonSerializerSettings
-            {
-                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
-            });
         }
 
         #region Scene Loaded Command
---
+++ UnitTests/SerializationTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using GameEngine.Core;
+using Newtonsoft.Json;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class SerializationTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            GameObject root = new GameObject("Root");
+            GameObject c1 = new GameObject("C1");
+            GameObject c2 = new GameObject("C2");
+            GameObject c3 = new GameObject("C3");
+
+            root.AddChild(c1);
+            root.AddChild(c2);
+            root.AddChild(c3);
+
+            string output = JsonConvert.SerializeObject(root, Formatting.Indented, new JsonSerializerSettings
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            });
+
+            // This almost deserializes correctly. We would just need to traverse the tree
+            // and set all the "parent" references.
+            GameObject loadedRoot = JsonConvert.DeserializeObject<GameObject>(output, new JsonSerializerSettings
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            });
+        }
+    }
+}
