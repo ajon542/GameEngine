29b45fe94b63836486007bedda3036c494dea0b2
Andrew Jones <ajon542@gmail.com>
2015-12-15 17:49:22 -0800

    Added changed default dimensions

---
+++ GameEngine/Core/Component.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine.Core
+{
+    class Component
+    {
+        // Renderer component
+        // ObjectBehavior component
+        // ShaderComponent for example.
+    }
+}
