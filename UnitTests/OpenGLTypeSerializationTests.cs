3b83ef59221828463605a022d6f4297420044d52
Andrew Jones <ajon542@gmail.com>
2015-12-26 18:43:43 -0800

    Separating tests
    Tests are going to have to be more involved. Serialization tests are to
    come first. Each time I add something to game object currently, it
    appears to break unit tests. This is good, but it makes it obvious that
    unit tests need to be the heart of the operation.

---
+++ UnitTests/OpenGLTypeSerializationTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using GameEngine.Core.Serialization;
+using OpenTK;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class OpenGLTypeSerializationTests
+    {
+        // TODO: Implement all the standard type converters as defined in:
+        // https://github.com/opentk/opentk/tree/develop/Source/OpenTK/Math
+        [TestMethod]
+        public void TestVector2Serialization()
+        {
+            Vector2 vec = new Vector2(1, 2);
+            ISerializer<Vector2> serializer = new Serializer<Vector2>();
+            string output = serializer.Serialize(vec);
+            Vector2 res = serializer.Deserialize(output);
+            Assert.AreEqual(vec, res);
+        }
+
+        [TestMethod]
+        public void TestVector3Serialization()
+        {
+            Vector3 vec = new Vector3(1, 2, 3);
+            ISerializer<Vector3> serializer = new Serializer<Vector3>();
+            string output = serializer.Serialize(vec);
+            Vector3 res = serializer.Deserialize(output);
+            Assert.AreEqual(vec, res);
+        }
+
+        [TestMethod]
+        public void TestQuaternionSerialization()
+        {
+            Quaternion qt = new Quaternion(1, 2, 3, 4);
+            ISerializer<Quaternion> serializer = new Serializer<Quaternion>();
+            string output = serializer.Serialize(qt);
+            Quaternion res = serializer.Deserialize(output);
+            Assert.AreEqual(qt, res);
+        }
+    }
+}
--- UnitTests/SerializationTests.cs
+++ UnitTests/SerializationTests.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-using GameEngine.Core;
 using GameEngine.Core.Serialization;
-using OpenTK;
 
 namespace UnitTests
 {
@@ -27,38 +23,6 @@ namespace UnitTests
             public string Name { get; set; }
         }
 
-        // TODO: Implement all the standard type converters as defined in:
-        // https://github.com/opentk/opentk/tree/develop/Source/OpenTK/Math
-        [TestMethod]
-        public void TestVector2Serialization()
-        {
-            Vector2 vec = new Vector2(1, 2);
-            ISerializer<Vector2> serializer = new Serializer<Vector2>();
-            string output = serializer.Serialize(vec);
-            Vector2 res = serializer.Deserialize(output);
-            Assert.AreEqual(vec, res);
-        }
-
-        [TestMethod]
-        public void TestVector3Serialization()
-        {
-            Vector3 vec = new Vector3(1, 2, 3);
-            ISerializer<Vector3> serializer = new Serializer<Vector3>();
-            string output = serializer.Serialize(vec);
-            Vector3 res = serializer.Deserialize(output);
-            Assert.AreEqual(vec, res);
-        }
-
-        [TestMethod]
-        public void TestQuaternionSerialization()
-        {
-            Quaternion qt = new Quaternion(1, 2, 3, 4);
-            ISerializer<Quaternion> serializer = new Serializer<Quaternion>();
-            string output = serializer.Serialize(qt);
-            Quaternion res = serializer.Deserialize(output);
-            Assert.AreEqual(qt, res);
-        }
-
         [TestMethod]
         public void TestDerivedSerialization()
         {
@@ -66,30 +30,6 @@ namespace UnitTests
             ISerializer<Base> serializer = new Serializer<Base>();
             string output = serializer.Serialize(b);
             Base res = serializer.Deserialize(output);
-        }
-
-        [TestMethod]
-        public void TestGameObjectParentChild()
-        {
-            GameObject root = new GameObject("Root");
-            GameObject c1 = new GameObject("C1");
-            c1.Transform.Position = new Vector3(1, 2, 3);
-            c1.Transform.Scale = new Vector3(4, 5, 6);
-            c1.Transform.Rotation = new Quaternion(7, 8, 9, 10);
-
-            root.AddChild(c1);
-
-            ISerializer<GameObject> serializer = new Serializer<GameObject>();
-
-            string output = serializer.Serialize(root);
-            GameObject loadedRoot = serializer.Deserialize(output);
-
-            List<GameObject> children = loadedRoot.GetChildren();
-
-            Assert.AreEqual(children.Count, 1);
-            Assert.AreEqual(c1.Transform, children[0].Transform);
-            Assert.AreEqual(root.Guid, loadedRoot.Guid);
-            Assert.AreEqual(c1.Guid, children[0].Guid);
         }
     }
 }
