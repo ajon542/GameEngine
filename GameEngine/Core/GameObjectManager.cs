f86bc6bdc49c25f5d34f8b72577e5b674a5fc6c5
Andrew Jones <ajon542@gmail.com>
2015-12-26 16:30:17 -0800

    added a game object manager.

---
+++ GameEngine/Core/GameObjectManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine.Core
+{
+    /// <summary>
+    /// The Game Object Manager is responsible for managing all the game objects
+    /// in a scene.
+    /// </summary>
+    /// <remarks>
+    /// Scenes can consist of a complex game object hierarchy, starting with a
+    /// single root game object linking to many child game objects. Being able
+    /// to manage and debug this hierarchy will be important.
+    /// </remarks>
+    class GameObjectManager
+    {
+    }
+}
