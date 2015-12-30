58218088a144a76294546d36153e384a6e2c0894
Andrew Jones <ajon542@gmail.com>
2015-12-30 14:18:30 -0800

    GameObjectManager static class
    After much thought I have decided that the GameObjectManager class was
    duplicating functionality already provided by the GameObject class. The
    methods must remain in the GameObject class so I made GameObjectManager
    static and decided it was going to be a helper class to do things like
    find child game objects in the hierarchy.

--- GameEngine/Core/GameObjectManager.cs
+++ GameEngine/Core/GameObjectManager.cs
@@ -3,78 +3,15 @@ using System.Collections.Generic;
 
 namespace GameEngine.Core
 {
-    /// <summary>
-    /// The Game Object Manager is responsible for managing all the game objects
-    /// in a scene.
-    /// </summary>
-    /// <remarks>
-    /// Scenes can consist of a complex game object hierarchy, starting with a
-    /// single root game object linking to many child game objects. Being able
-    /// to manage and debug this hierarchy will be important.
-    /// While it is possible to modify the game object hierarchy by following the
-    /// game object parent and child links, this class will provide some of the
-    /// common tasks needed.
-    /// </remarks>
-    class GameObjectManager
+    public static class GameObjectManager
     {
-        /// <summary>
-        /// The root game object. A scene will always have one of these.
-        /// </summary>
-        private GameObject root = new GameObject();
-
-        /// <summary>
-        /// Add a child to the game object with the given guid.
-        /// </summary>
-        /// <param name="guid">The guid of the parent object.</param>
-        /// <param name="child">The child object to add.</param>
-        public void AddChild(Guid guid, GameObject child)
-        {
-            GameObject gameObject = FindGameObject(guid);
-
-            if (gameObject != null)
-            {
-                gameObject.AddChild(child);
-            }
-        }
-
-        /// <summary>
-        /// Add a list of children to the game object with the given guid.
-        /// </summary>
-        /// <param name="guid">The guid of the parent object.</param>
-        /// <param name="children">The list of child objects to add.</param>
-        public void AddChildren(Guid guid, List<GameObject> children)
-        {
-            GameObject gameObject = FindGameObject(guid);
-
-            if (gameObject != null)
-            {
-                foreach (GameObject child in children)
-                {
-                    gameObject.AddChild(child);
-                }
-            }
-        }
-
-        /// <summary>
-        /// Remove all children from their parent.
-        /// </summary>
-        /// <param name="guid">The guid of the parent object.</param>
-        public void RemoveChildren(Guid guid)
-        {
-            GameObject gameObject = FindGameObject(guid);
-
-            if (gameObject != null)
-            {
-                gameObject.RemoveChildren();
-            }
-        }
-
         /// <summary>
         /// Find the game object with the given guid.
         /// </summary>
+        /// <param name="root">The root game object to start searching from.</param>
         /// <param name="guid">The guid to find.</param>
-        /// <returns>The game object with the guid or null.</returns>
-        public GameObject FindGameObject(Guid guid)
+        /// <returns>The game object with the matching guid or null.</returns>
+        public static GameObject FindGameObject(GameObject root, Guid guid)
         {
             Queue<GameObject> unvisited = new Queue<GameObject>();
             unvisited.Enqueue(root);
--- UnitTests/GameObjectTests.cs
+++ UnitTests/GameObjectTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using GameEngine.Core;
 
@@ -16,6 +17,59 @@ namespace UnitTests
             Assert.IsNull(go.GetComponent<Mesh>());
             go.AddComponent<Mesh>(mesh);
             Assert.IsNotNull(go.GetComponent<Mesh>());
+        }
+
+        [TestMethod]
+        public void TestGameObjectManagerChildAddition()
+        {
+            GameObject go = new GameObject();
+
+            GameObject c1 = new GameObject();
+            GameObject cc1 = new GameObject();
+            GameObject cc2 = new GameObject();
+            c1.AddChild(cc1);
+            c1.AddChild(cc2);
+
+            GameObject c2 = new GameObject();
+            GameObject c3 = new GameObject();
+
+            go.AddChild(c1);
+            go.AddChild(c2);
+            go.AddChild(c3);
+
+            // Check that all children are found.
+            Assert.IsNotNull(GameObjectManager.FindGameObject(go, c1.Guid));
+            Assert.IsNotNull(GameObjectManager.FindGameObject(go, cc1.Guid));
+            Assert.IsNotNull(GameObjectManager.FindGameObject(go, cc2.Guid));
+            Assert.IsNotNull(GameObjectManager.FindGameObject(go, c2.Guid));
+            Assert.IsNotNull(GameObjectManager.FindGameObject(go, c3.Guid));
+
+            // Check that a child that was not added, is not found.
+            Assert.IsNull(GameObjectManager.FindGameObject(go, Guid.NewGuid()));
+        }
+
+        [TestMethod]
+        public void TestGameObjectManagerChildRemoval()
+        {
+            GameObject go = new GameObject();
+
+            GameObject c1 = new GameObject();
+            GameObject cc1 = new GameObject();
+            GameObject cc2 = new GameObject();
+            c1.AddChild(cc1);
+            c1.AddChild(cc2);
+            go.AddChild(c1);
+
+            // Remove the children.
+            c1.RemoveChildren();
+
+            // Check that children aren't found.
+            Assert.IsNull(GameObjectManager.FindGameObject(go, cc1.Guid));
+            Assert.IsNull(GameObjectManager.FindGameObject(go, cc2.Guid));
+
+            // Remove the children.
+            go.RemoveChildren();
+            Assert.IsNull(GameObjectManager.FindGameObject(go, c1.Guid));
         }
     }
 }
