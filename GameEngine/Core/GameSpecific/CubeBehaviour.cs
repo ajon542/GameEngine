7b9b661f23897e3f4b88abaff2c10a22e4929e93
Andrew Jones <ajon542@gmail.com>
2015-12-30 21:06:39 -0800

    cleaned up cube behaviour

--- GameEngine/Core/GameSpecific/CubeBehaviour.cs
+++ GameEngine/Core/GameSpecific/CubeBehaviour.cs
@@ -13,40 +13,33 @@ namespace GameEngine.Core.GameSpecific
         public override void Initialize()
         {
             mesh = new Mesh();
-            mesh.Vertices.Add(new Vector3(-0.5f, -0.5f, -0.5f));
-            mesh.Vertices.Add(new Vector3(0.5f, -0.5f, -0.5f));
-            mesh.Vertices.Add(new Vector3(0.5f, 0.5f, -0.5f));
-            mesh.Vertices.Add(new Vector3(-0.5f, 0.5f, -0.5f));
-            mesh.Vertices.Add(new Vector3(-0.5f, -0.5f, 0.5f));
-            mesh.Vertices.Add(new Vector3(0.5f, -0.5f, 0.5f));
-            mesh.Vertices.Add(new Vector3(0.5f, 0.5f, 0.5f));
-            mesh.Vertices.Add(new Vector3(-0.5f, 0.5f, 0.5f));
 
-            int[] indices =
+            mesh.Vertices = new List<Vector3>
             {
-                //left
-                0, 2, 1,
-                0, 3, 2,
-                //back
-                1, 2, 6,
-                6, 5, 1,
-                //right
-                4, 5, 6,
-                6, 7, 4,
-                //top
-                2, 3, 6,
-                6, 3, 7,
-                //front
-                0, 7, 3,
-                0, 4, 7,
-                //bottom
-                0, 1, 5,
-                0, 5, 4
+                new Vector3(-0.5f, -0.5f, -0.5f),
+                new Vector3(0.5f, -0.5f, -0.5f),
+                new Vector3(0.5f, 0.5f, -0.5f),
+                new Vector3(-0.5f, 0.5f, -0.5f),
+                new Vector3(-0.5f, -0.5f, 0.5f),
+                new Vector3(0.5f, -0.5f, 0.5f),
+                new Vector3(0.5f, 0.5f, 0.5f),
+                new Vector3(-0.5f, 0.5f, 0.5f)
             };
 
-            mesh.Triangles = new List<int>(indices);
-            mesh.Colours = new List<Vector3>();
-            mesh.Colours.Add(Colour);
+            mesh.Triangles = new List<int>
+            {
+                0, 2, 1, 0, 3, 2, // left
+                1, 2, 6, 6, 5, 1, // back
+                4, 5, 6, 6, 7, 4, // right
+                2, 3, 6, 6, 3, 7, // top
+                0, 7, 3, 0, 4, 7, // front
+                0, 1, 5, 0, 5, 4  // bottom
+            };
+
+            mesh.Colours = new List<Vector3>
+            {
+                Colour
+            };
 
             gameObject.AddComponent<Mesh>(mesh);
 
