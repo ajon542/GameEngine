a21f7fe8ae2215739945160b4d907559267e7424
Andrew Jones <ajon542@gmail.com>
2015-12-31 08:12:02 -0800

    Cleaned up the example

--- GameEngine/Core/GameSpecific/Tutorial2.cs
+++ GameEngine/Core/GameSpecific/Tutorial2.cs
@@ -1,56 +1,66 @@
 ﻿using System;
-using System.Diagnostics;
 using OpenTK;
-using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL;
 
 namespace GameEngine.Core.GameSpecific
 {
+    // http://www.opengl-tutorial.org/beginners-tutorials/tutorial-2-the-first-triangle/
     class Tutorial2 : Scene
     {
-        // This will identify our vertex buffer
+        /// <summary>
+        /// Vertex array object identifier.
+        /// </summary>
+        uint vertexArrayId;
+
+        /// <summary>
+        /// Vertex buffer identifier.
+        /// </summary>
         uint vertexbuffer;
-        uint VertexArrayID;
-        float[] g_vertex_buffer_data = new float[]{
-            -1.0f, -1.0f, -3.0f,
-            1.0f, -1.0f, -3.0f,
-            0.0f,  1.0f, -3.0f,
-            };
+
+        /// <summary>
+        /// Vertex buffer data.
+        /// </summary>
+        float[] vertexBufferData =
+        {
+            -1.0f, -1.0f, -5.0f,
+             1.0f, -1.0f, -5.0f,
+             0.0f,  1.0f, -5.0f,
+        };
 
         public override void Initialize()
         {
-            // Other state
-            GL.Enable(EnableCap.DepthTest);
-            GL.ClearColor(0, 0.1f, 0.4f, 1);
+            // Generate a VAO and set it as the current one.
+            GL.GenVertexArrays(1, out vertexArrayId);
+            GL.BindVertexArray(vertexArrayId);
 
-            GL.GenVertexArrays(1, out VertexArrayID);
-            GL.BindVertexArray(VertexArrayID);
-
-            // Generate 1 buffer, put the resulting identifier in vertexbuffer
+            // Generate 1 buffer, put the resulting identifier in vertexbuffer.
             GL.GenBuffers(1, out vertexbuffer);
-            // The following commands will talk about our 'vertexbuffer' buffer
+            // The following commands will talk about our 'vertexbuffer' buffer.
             GL.BindBuffer(BufferTarget.ArrayBuffer, vertexbuffer);
             // Give our vertices to OpenGL.
-            IntPtr vertexBufferSize = (IntPtr)(g_vertex_buffer_data.Length * Vector3.SizeInBytes);
-            GL.BufferData(BufferTarget.ArrayBuffer, vertexBufferSize, g_vertex_buffer_data, BufferUsageHint.StaticDraw);
+            IntPtr vertexBufferSize = (IntPtr)(vertexBufferData.Length * Vector3.SizeInBytes);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertexBufferSize, vertexBufferData, BufferUsageHint.StaticDraw);
         }
 
 
         public override void Render()
         {
-            // 1rst attribute buffer : vertices
+            // First attribute buffer - vertices
             GL.EnableVertexAttribArray(0);
             GL.BindBuffer(BufferTarget.ArrayBuffer, vertexbuffer);
+
             GL.VertexAttribPointer(
                 0,                              // attribute 0. No particular reason for 0, but must match the layout in the shader.
                 3,                              // size
                 VertexAttribPointerType.Float,  // type
-                false,                          // normalized?
+                false,                          // normalized
                 0,                              // stride
                 0                               // array buffer offset
                 );
-            // Draw the triangle !
-            GL.DrawArrays(PrimitiveType.Triangles, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle 
+
+            // Draw the triangle.
+            // Starting from vertex 0; 3 vertices total -> 1 triangle .
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
             GL.DisableVertexAttribArray(0);
 
             GL.Flush();
