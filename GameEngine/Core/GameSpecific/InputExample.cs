9749f596e35299ad730d63cf52249b4c1bfe415d
Andrew Jones <ajon542@gmail.com>
2016-01-01 09:25:39 -0800

    Added an input example

---
+++ GameEngine/Core/GameSpecific/InputExample.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+
+using GameEngine.Core.Graphics;
+
+namespace GameEngine.Core.GameSpecific
+{
+    public class InputExample : Scene
+    {
+        private int mouseWheelIndex = 0;
+
+        public override void Update()
+        {
+            var mouse = Mouse.GetState();
+            if (mouse[MouseButton.Left])
+            {
+                Console.WriteLine("mouse button pressed");
+            }
+            var keyboard = Keyboard.GetState();
+            if (keyboard[Key.A])
+            {
+                Console.WriteLine("Key.A was pressed");
+            }
+
+            if (mouseWheelIndex != mouse.Wheel)
+            {
+                mouseWheelIndex = mouse.Wheel;
+                Console.WriteLine(mouse.Wheel);
+            }
+        }
+    }
+}
