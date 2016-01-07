c278fa5e5b16202a951643c6d1322748666d5529
Andrew Jones <ajon542@gmail.com>
2016-01-06 20:10:28 -0800

    comments

--- GameEngine/Core/Shaders/vert.glsl
+++ GameEngine/Core/Shaders/vert.glsl
@@ -1,5 +1,8 @@
 ﻿#version 330
  
+// These attributes are what the stream must provide in order to render
+// with this shader. i.e. it must provide a vector of positions and colors.
+// All of these arrays must have the same number of elements.
 in vec3 vPosition;
 in vec3 vColor;
 out vec4 color;
