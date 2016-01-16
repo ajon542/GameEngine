25e8985cfbe6847abfe49ef5bf01fc8dc59f93a7
Andrew Jones <ajon542@gmail.com>
2016-01-15 22:53:11 -0800

    Added quad mesh

--- GameEngine/Core/GameSpecific/QuadBehaviour.cs
+++ GameEngine/Core/GameSpecific/QuadBehaviour.cs
@@ -8,8 +8,6 @@ namespace GameEngine.Core.GameSpecific
 {
     class QuadBehaviour : Behaviour
     {
-        private Mesh mesh;
-
         public override void Initialize()
         {
             if (GameObject == null)
@@ -20,39 +18,7 @@ namespace GameEngine.Core.GameSpecific
                 throw new GameEngineException("game object null");
             }
 
-            mesh = new Mesh();
-
-            mesh.Vertices = new List<Vector3>
-            {
-                new Vector3(-1.0f, -1.0f, 0.0f),
-                new Vector3(1.0f, -1.0f, 0.0f),
-                new Vector3(1.0f,  1.0f, 0.0f),
-                new Vector3(1.0f,  1.0f, 0.0f),
-                new Vector3(-1.0f,  1.0f, 0.0f),
-                new Vector3(-1.0f, -1.0f, 0.0f),
-            };
-
-            /// <summary>
-            /// UV buffer data.
-            /// </summary>
-            /// <remarks>
-            /// 0,0 --- 1,0
-            ///  |       |
-            ///  |       |
-            /// 0,1 --- 1,1
-            /// </remarks>
-            mesh.UV = new List<Vector2>
-            {
-                new Vector2(0, 1),
-                new Vector2(1, 1),
-                new Vector2(1, 0),
-                new Vector2(1, 0),
-                new Vector2(0, 0),
-                new Vector2(0, 1),
-            };
-
-            GameObject.AddComponent<Mesh>(mesh);
-
+            GameObject.AddComponent<Mesh>(new Quad());
             GameObject.Transform.Position = new Vector3(0, 0, -3.0f);
         }
     }
---
+++ GameEngine/Core/Quad.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace GameEngine.Core
+{
+    class Quad : Mesh
+    {
+        public Quad()
+        {
+            Vertices = new List<Vector3>
+            {
+                new Vector3(-1.0f, -1.0f, 0.0f),
+                new Vector3(1.0f, -1.0f, 0.0f),
+                new Vector3(1.0f,  1.0f, 0.0f),
+                new Vector3(1.0f,  1.0f, 0.0f),
+                new Vector3(-1.0f,  1.0f, 0.0f),
+                new Vector3(-1.0f, -1.0f, 0.0f),
+            };
+
+            /// <summary>
+            /// UV buffer data.
+            /// </summary>
+            /// <remarks>
+            /// 0,0 --- 1,0
+            ///  |       |
+            ///  |       |
+            /// 0,1 --- 1,1
+            /// </remarks>
+            UV = new List<Vector2>
+            {
+                new Vector2(0, 1),
+                new Vector2(1, 1),
+                new Vector2(1, 0),
+                new Vector2(1, 0),
+                new Vector2(0, 0),
+                new Vector2(0, 1),
+            };
+        }
+    }
+}
