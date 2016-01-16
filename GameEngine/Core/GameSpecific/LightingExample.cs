03b191950296d64fcc5295b09d8a0063e9d9b439
Andrew Jones <ajon542@gmail.com>
2016-01-16 11:47:30 -0800

    Added directional lighting
    TODO: Calculate the normal matrix correctly. It looks like there are
    some bugs :)

--- GameEngine/Core/GameSpecific/LightingExample.cs
+++ GameEngine/Core/GameSpecific/LightingExample.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 using OpenTK;
 using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
 
 using GameEngine.Core.Graphics;
 
@@ -19,6 +20,8 @@ namespace GameEngine.Core.GameSpecific
         private int positionAttr;
         private uint colourBuffer;
         private int colourAttr;
+        private uint normalBuffer;
+        private int normalAttr;
         private uint elementBuffer;
 
         private PrimitiveType renderType = PrimitiveType.Triangles;
@@ -31,11 +34,13 @@ namespace GameEngine.Core.GameSpecific
 
         public override void Initialize()
         {
-            shaders.Add("default", new ShaderProgram("Core/Shaders/ambient-vert.glsl", "Core/Shaders/ambient-frag.glsl", true));
+            shaders.Add("default", new ShaderProgram("Core/Shaders/directional-vert.glsl", "Core/Shaders/directional-frag.glsl", true));
             positionBuffer = shaders["default"].GetBuffer("VertexPosition");
             positionAttr = shaders["default"].GetAttribute("VertexPosition");
             colourBuffer = shaders["default"].GetBuffer("VertexColor");
             colourAttr = shaders["default"].GetAttribute("VertexColor");
+            normalBuffer = shaders["default"].GetBuffer("VertexNormal");
+            normalAttr = shaders["default"].GetAttribute("VertexNormal");
 
             GL.GenVertexArrays(1, out vertexArrObject);
             GL.BindVertexArray(vertexArrObject);
@@ -43,10 +48,12 @@ namespace GameEngine.Core.GameSpecific
 
             Vector3[] vertices = mesh.Vertices.ToArray();
             Vector3[] colours = mesh.Colours.ToArray();
+            Vector3[] normals = mesh.Normals.ToArray();
             int[] indices = mesh.Indices.ToArray();
 
             IntPtr verticesLength = (IntPtr)(vertices.Length * Vector3.SizeInBytes);
             IntPtr coloursLength = (IntPtr)(colours.Length * Vector3.SizeInBytes);
+            IntPtr normalsLength = (IntPtr)(normals.Length * Vector3.SizeInBytes);
             IntPtr indicesLength = (IntPtr)(indices.Length * sizeof(int));
             indicesCount = indices.Length;
 
@@ -58,11 +65,35 @@ namespace GameEngine.Core.GameSpecific
             GL.BufferData(BufferTarget.ArrayBuffer, coloursLength, colours, BufferUsageHint.StaticDraw);
             GL.VertexAttribPointer(colourAttr, 3, VertexAttribPointerType.Float, true, 0, 0);
 
+            GL.BindBuffer(BufferTarget.ArrayBuffer, normalBuffer);
+            GL.BufferData(BufferTarget.ArrayBuffer, normalsLength, normals, BufferUsageHint.StaticDraw);
+            GL.VertexAttribPointer(normalAttr, 3, VertexAttribPointerType.Float, false, 0, 0);
+
             GL.BindBuffer(BufferTarget.ElementArrayBuffer, elementBuffer);
             GL.BufferData(BufferTarget.ElementArrayBuffer, indicesLength, indices, BufferUsageHint.StaticDraw);
+        }
+
+        public float xRot = 0;
+        public float yRot = 0;
+        public float zRot = 0;
+        public override void Update()
+        {
+            var keyboard = Keyboard.GetState();
+            if (keyboard[Key.X])
+            {
+                xRot += 0.1f;
+            }
+            if (keyboard[Key.Y])
+            {
+                yRot += 0.1f;
+            }
+            if (keyboard[Key.Z])
+            {
+                zRot += 0.1f;
+            }
 
             gameObject.Transform.Position = new Vector3(0, 0, -5);
-            gameObject.Transform.Rotation = new Quaternion(1, 1, 0, 1);
+            gameObject.Transform.Rotation = new Quaternion(xRot, yRot, zRot, 1);
             gameObject.CalculateModelMatrix();
             gameObject.ViewProjectionMatrix = cam.ViewMatrix * ProjectionMatrix;
             gameObject.ModelViewProjectionMatrix = gameObject.ModelMatrix * gameObject.ViewProjectionMatrix;
@@ -76,7 +107,18 @@ namespace GameEngine.Core.GameSpecific
 
             shaders["default"].EnableVertexAttribArrays();
 
-            GL.Uniform4(shaders["default"].GetUniform("Ambient"), new Vector4(0.5f, 0.5f, 0.5f, 1));
+            // TODO: Determine good values for the directional lighting.
+            GL.Uniform3(shaders["default"].GetUniform("Ambient"), new Vector3(0.5f, 0.5f, 0.5f));
+            GL.Uniform3(shaders["default"].GetUniform("LightColor"), new Vector3(1, 1, 1));
+            GL.Uniform3(shaders["default"].GetUniform("LightDirection"), new Vector3(1, 1, 1));
+            GL.Uniform3(shaders["default"].GetUniform("HalfVector"), new Vector3(0, 0, 0));
+            GL.Uniform1(shaders["default"].GetUniform("Shininess"), 1.0f);
+            GL.Uniform1(shaders["default"].GetUniform("Strength"), 1.0f);
+
+            // TODO: Calculate the normal matrix correctly.
+            // See: http://www.lighthouse3d.com/tutorials/glsl-12-tutorial/the-normal-matrix/
+            Matrix4 modelViewMatrix = gameObject.ModelMatrix * cam.ViewMatrix;
+            GL.UniformMatrix4(shaders["default"].GetUniform("NormalMatrix"), false, ref modelViewMatrix);
             GL.UniformMatrix4(shaders["default"].GetUniform("MVPMatrix"), false, ref gameObject.ModelViewProjectionMatrix);
             GL.DrawElements(renderType, indicesCount, DrawElementsType.UnsignedInt, 0);
 
--- GameEngine/Core/Shaders/directional-vert.glsl
+++ GameEngine/Core/Shaders/directional-vert.glsl
@@ -1,14 +1,20 @@
 ﻿#version 330
 
+in vec3 VertexNormal;
 in vec3 VertexPosition;
 in vec3 VertexColor;
+
 out vec4 Color;
+out vec3 Normal;
+
 uniform mat4 MVPMatrix;
+uniform mat4 NormalMatrix;
  
 void
 main()
 {
-    gl_Position = MVPMatrix * vec4(VertexPosition, 1.0);
- 
+    mat3 normalMatrix = mat3(NormalMatrix);
     Color = vec4(VertexColor, 1.0);
+    Normal = normalize(normalMatrix * VertexNormal);
+    gl_Position = MVPMatrix * vec4(VertexPosition, 1.0);
 }
