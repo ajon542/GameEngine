bfd1b0da3700b6c2b93b82da3a120c33d436f216
Andrew Jones <ajon542@gmail.com>
2016-01-20 18:53:14 -0800

    Clean up and comments.

--- GameEngine/Core/Torus.cs
+++ GameEngine/Core/Torus.cs
@@ -10,12 +10,26 @@ namespace GameEngine.Core
     /// </summary>
     class Torus : Mesh
     {
-        public Torus()
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Torus"/> mesh class.
+        /// </summary>
+        /// <param name="radius">The radius of the torus.</param>
+        /// <param name="tubeRadius">The radius of the tube.</param>
+        /// <param name="subDivAround">Sub-divisions around the torus.</param>
+        /// <param name="subDivTube">Sub-divisions around the tube.</param>
+        public Torus(float radius, float tubeRadius, int subDivAround, int subDivTube)
         {
-            GenerateTorus(7.0f, 2.0f, 20, 20);
+            GenerateTorus(radius, tubeRadius, subDivAround, subDivTube);
         }
 
-        public void GenerateTorus(float fRadius, float fTubeRadius, int iSubDivAround, int iSubDivTube)
+        /// <summary>
+        /// Generates a torus mesh.
+        /// </summary>
+        /// <param name="radius">The radius of the torus.</param>
+        /// <param name="tubeRadius">The radius of the tube.</param>
+        /// <param name="subDivAround">Sub-divisions around the torus.</param>
+        /// <param name="subDivTube">Sub-divisions around the tube.</param>
+        public void GenerateTorus(float radius, float tubeRadius, int subDivAround, int subDivTube)
         {
             Vertices = new List<Vector3>();
             Normals = new List<Vector3>();
@@ -23,15 +37,15 @@ namespace GameEngine.Core
             UV = new List<Vector2>();
             Indices = new List<int>();
 
-            float fAddAngleAround = 360.0f / (float)iSubDivAround;
-            float fAddAngleTube = 360.0f / (float)iSubDivTube;
+            float fAddAngleAround = 360.0f / (float)subDivAround;
+            float fAddAngleTube = 360.0f / (float)subDivTube;
 
             float fCurAngleAround = 0.0f;
             int iStepsAround = 1;
 
             int iFacesAdded = 0;
 
-            while (iStepsAround <= iSubDivAround)
+            while (iStepsAround <= subDivAround)
             {
                 float fSineAround = (float)Math.Sin(fCurAngleAround / 180.0f * Math.PI);
                 float fCosineAround = (float)Math.Cos(fCurAngleAround / 180.0f * Math.PI);
@@ -42,20 +56,21 @@ namespace GameEngine.Core
                 Vector3 vDir2 = new Vector3(fNextSineAround, fNextCosineAround, 0.0f);
                 float fCurAngleTube = 0.0f;
                 int iStepsTube = 1;
-                while (iStepsTube <= iSubDivTube)
+
+                while (iStepsTube <= subDivTube)
                 {
                     float fSineTube = (float)Math.Sin(fCurAngleTube / 180.0f * Math.PI);
                     float fCosineTube = (float)Math.Cos(fCurAngleTube / 180.0f * Math.PI);
                     float fNextAngleTube = fCurAngleTube + fAddAngleTube;
                     float fNextSineTube = (float)Math.Sin(fNextAngleTube / 180.0f * Math.PI);
                     float fNextCosineTube = (float)Math.Cos(fNextAngleTube / 180.0f * Math.PI);
-                    Vector3 vMid1 = vDir1 * new Vector3(fRadius - fTubeRadius / 2), vMid2 = vDir2 * (fRadius - fTubeRadius / 2);
+                    Vector3 vMid1 = vDir1 * new Vector3(radius - tubeRadius / 2), vMid2 = vDir2 * (radius - tubeRadius / 2);
                     Vector3[] vQuadPoints = 
                     {
-                        vMid1 + new Vector3(0.0f, 0.0f, -fNextSineTube*fTubeRadius) + vDir1*fNextCosineTube*fTubeRadius,
-                        vMid1 + new Vector3(0.0f, 0.0f, -fSineTube*fTubeRadius) + vDir1*fCosineTube*fTubeRadius,
-                        vMid2 + new Vector3(0.0f, 0.0f, -fSineTube*fTubeRadius) + vDir2*fCosineTube*fTubeRadius,
-                        vMid2 + new Vector3(0.0f, 0.0f, -fNextSineTube*fTubeRadius) + vDir2*fNextCosineTube*fTubeRadius
+                        vMid1 + new Vector3(0.0f, 0.0f, -fNextSineTube*tubeRadius) + vDir1*fNextCosineTube*tubeRadius,
+                        vMid1 + new Vector3(0.0f, 0.0f, -fSineTube*tubeRadius) + vDir1*fCosineTube*tubeRadius,
+                        vMid2 + new Vector3(0.0f, 0.0f, -fSineTube*tubeRadius) + vDir2*fCosineTube*tubeRadius,
+                        vMid2 + new Vector3(0.0f, 0.0f, -fNextSineTube*tubeRadius) + vDir2*fNextCosineTube*tubeRadius
                     };
 
                     Vector3[] vNormals =
@@ -85,7 +100,7 @@ namespace GameEngine.Core
                         Colours.Add(new Vector3(0.9f, 0.9f, 0.9f));
                     }
 
-                    iFacesAdded += 2; // Keep count of added faces
+                    iFacesAdded += 2;
                     fCurAngleTube += fAddAngleTube;
                     iStepsTube++;
                 }
