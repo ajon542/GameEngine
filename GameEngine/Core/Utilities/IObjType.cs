30c531c34dd859e6849be30ff665439fa8c85b61
Andrew Jones <ajon542@gmail.com>
2016-01-25 10:26:43 -0800

    Add file structure for reading obj files

---
+++ GameEngine/Core/Utilities/IObjType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine.Core.Utilities
+{
+    interface IObjType
+    {
+        void Deserialize(string input);
+        string Serialize();
+    }
+}
---
+++ GameEngine/Core/Utilities/Normal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine.Core.Utilities
+{
+    class Normal : IObjType
+    {
+        public void Deserialize(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Serialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
