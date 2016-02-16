83b692ae060fd82eb213ef74da88286b23f99d88
Andrew Jones <ajon542@gmail.com>
2016-02-15 21:32:39 -0800

    added component and projection calcs

--- GameEngine/Core/Utilities/VectorExtensions.cs
+++ GameEngine/Core/Utilities/VectorExtensions.cs
@@ -6,27 +6,20 @@ namespace GameEngine.Core.Utilities
     public static class VectorExtensions
     {
         // http://math.oregonstate.edu/home/programs/undergrad/CalculusQuestStudyGuides/vcalc/dotprod/dotprod.html
-        public static int Component(Vector3 a, Vector3 b)
+        public static float Comp(Vector3 a, Vector3 b)
         {
-            throw new NotImplementedException();
+            return Vector3.Dot(a, b) / a.Length;
         }
 
-        public static Vector3 Project(Vector3 a, Vector3 b)
+        public static Vector3 Proj(Vector3 a, Vector3 b)
         {
-            throw new NotImplementedException();
-        }
-
-        public static Vector3 Project(Vector3 a, Vector3 b)
-        {
-            throw new NotImplementedException();
+            return Comp(a, b) * (a / a.Length);
         }
         
         public static Vector3 Reflect(Vector3 l, Vector3 n)
         {
-            //R = 2(N dot L)N - L
             Vector3 r = 2 * Vector3.Dot(n, l) * n - l;
             return r;
         }
-
     }
 }
