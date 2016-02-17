3466871746aad0824ab40be5f109f684bec2d883
Andrew Jones <ajon542@gmail.com>
2016-02-16 20:08:24 -0800

    Added updated phong shading example from superbible

---
+++ GameEngine/Core/GameSpecific/PhongExample.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using GameEngine.Core.Utilities.ObjParser;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+
+using GameEngine.Core.Graphics;
+
+namespace GameEngine.Core.GameSpecific
+{
+    /// <summary>
+    /// Example to demonstrate Phong shading.
+    /// </summary>
+    public class PhongExample : Scene
+    {
+        private uint vertexArrObject;
+        private int indicesCount;
+        private uint positionBuffer;
+        private int positionAttr;
+        private uint normalBuffer;
+        private int normalAttr;
+        private uint elementBuffer;
+
+        private GameObject gameObject = new GameObject();
+        private Mesh mesh = new Sphere(4, 2);
+        private Dictionary<string, ShaderProgram> shaders = new Dictionary<string, ShaderProgram>();
+
+        public override void Initialize()
+        {
+            shaders.Add("default", new ShaderProgram("Core/Shaders/phong43-vert.glsl", "Core/Shaders/phong43-frag.glsl", true));
+
+            positionBuffer = shaders["default"].GetBuffer("position");
+            positionAttr = shaders["default"].GetAttribute("position");
+            normalBuffer = shaders["default"].GetBuffer("normal");
+            normalAttr = shaders["default"].GetAttribute("normal");
+
+            GL.GenVertexArrays(1, out vertexArrObject);
+            GL.BindVertexArray(vertexArrObject);
+            GL.GenBuffers(1, out elementBuffer);
+
+            Vector3[] vertices = mesh.Vertices.ToArray();
+            Vector3[] normals = mesh.Normals.ToArray();
+            int[] indices = mesh.Indices.ToArray();
+
+            IntPtr verticesLength = (IntPtr)(vertices.Length * Vector3.SizeInBytes);
+            IntPtr normalsLength = (IntPtr)(normals.Length * Vector3.SizeInBytes);
+            IntPtr indicesLength = (IntPtr)(indices.Length * sizeof(int));
+            indicesCount = indices.Length;
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, positionBuffer);
+            GL.BufferData(BufferTarget.ArrayBuffer, verticesLength, vertices, BufferUsageHint.StaticDraw);
+            GL.VertexAttribPointer(positionAttr, 3, VertexAttribPointerType.Float, false, 0, 0);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, normalBuffer);
+            GL.BufferData(BufferTarget.ArrayBuffer, normalsLength, normals, BufferUsageHint.StaticDraw);
+            GL.VertexAttribPointer(normalAttr, 3, VertexAttribPointerType.Float, false, 0, 0);
+
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, elementBuffer);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, indicesLength, indices, BufferUsageHint.StaticDraw);
+
+            GL.Enable(EnableCap.DepthTest);
+            GL.Enable(EnableCap.CullFace);
+        }
+
+        public override void Update()
+        {
+            MainCamera.Update();
+            gameObject.Transform.Position = new Vector3(0, 0, -10);
+            gameObject.CalculateModelMatrix();
+            gameObject.ViewProjectionMatrix = MainCamera.ViewMatrix * MainCamera.ProjectionMatrix;
+            gameObject.ModelViewProjectionMatrix = gameObject.ModelMatrix * gameObject.ViewProjectionMatrix;
+        }
+
+        public override void Render()
+        {
+            GL.UseProgram(shaders["default"].ProgramId);
+
+            GL.BindVertexArray(vertexArrObject);
+
+            shaders["default"].EnableVertexAttribArrays();
+
+            Matrix4 modelViewMatrix = gameObject.ModelMatrix * MainCamera.ViewMatrix;
+            Matrix4 projectionMatrix = MainCamera.ProjectionMatrix;
+
+            GL.UniformMatrix4(shaders["default"].GetUniform("mv_matrix"), false, ref modelViewMatrix);
+            GL.UniformMatrix4(shaders["default"].GetUniform("proj_matrix"), false, ref projectionMatrix);
+
+            GL.DrawElements(mesh.RenderType, indicesCount, DrawElementsType.UnsignedInt, 0);
+
+            shaders["default"].DisableVertexAttribArrays();
+        }
+    }
+}
---
+++ GameEngine/Core/Shaders/phong43-vert.glsl
@@ -0,0 +1,36 @@
+﻿#version 430 core
+
+layout (location = 0) in vec3 position;
+layout (location = 1) in vec3 normal;
+
+uniform mat4 mv_matrix;
+uniform mat4 proj_matrix;
+
+// Outputs to the fragment shader
+out Fragment
+{
+    vec3 N;
+    vec3 L;
+    vec3 V;
+} fragment;
+
+// Position of light
+uniform vec3 light_pos = vec3(100.0, 100.0, 100.0);
+
+void main(void)
+{
+    // Calculate view-space coordinate
+    vec4 P = mv_matrix * vec4(position, 1);
+
+    // Calculate normal in view-space
+    fragment.N = mat3(mv_matrix) * normal;
+
+    // Calculate light vector
+    fragment.L = light_pos - P.xyz;
+
+    // Calculate view vector
+    fragment.V = -P.xyz;
+
+    // Calculate the clip-space position of each vertex
+    gl_Position = proj_matrix * P;
+}
