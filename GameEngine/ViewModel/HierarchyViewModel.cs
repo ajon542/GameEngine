f8c665cea9f8efe360b5b44a4aa888c11f83081c
Andrew Jones <ajon542@gmail.com>
2016-08-17 17:17:29 -0700

    Added Hierarchy view example
    This contains some classes only for example. They should probably be
    removed. This took a while to get working so I'm committing it.

--- GameEngine/ViewModel/HierarchyViewModel.cs
+++ GameEngine/ViewModel/HierarchyViewModel.cs
@@ -1,7 +1,411 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+
 namespace GameEngine.ViewModel
 {
+    public class Person
+    {
+        readonly List<Person> _children = new List<Person>();
+        public IList<Person> Children
+        {
+            get { return _children; }
+        }
+
+        public string Name { get; set; }
+    }
+
+    /// <summary>
+    /// A UI-friendly wrapper around a Person object.
+    /// </summary>
+    public class PersonViewModel : INotifyPropertyChanged
+    {
+        #region Data
+
+        readonly ReadOnlyCollection<PersonViewModel> _children;
+        readonly PersonViewModel _parent;
+        readonly Person _person;
+
+        bool _isExpanded;
+        bool _isSelected;
+
+        #endregion // Data
+
+        #region Constructors
+
+        public PersonViewModel(Person person)
+            : this(person, null)
+        {
+        }
+
+        private PersonViewModel(Person person, PersonViewModel parent)
+        {
+            _person = person;
+            _parent = parent;
+
+            _children = new ReadOnlyCollection<PersonViewModel>(
+                    (from child in _person.Children
+                     select new PersonViewModel(child, this))
+                     .ToList<PersonViewModel>());
+        }
+
+        #endregion // Constructors
+
+        #region Person Properties
+
+        public ReadOnlyCollection<PersonViewModel> Children
+        {
+            get { return _children; }
+        }
+
+        public string Name
+        {
+            get { return _person.Name; }
+        }
+
+        #endregion // Person Properties
+
+        #region Presentation Members
+
+        #region IsExpanded
+
+        /// <summary>
+        /// Gets/sets whether the TreeViewItem 
+        /// associated with this object is expanded.
+        /// </summary>
+        public bool IsExpanded
+        {
+            get { return _isExpanded; }
+            set
+            {
+                if (value != _isExpanded)
+                {
+                    _isExpanded = value;
+                    this.OnPropertyChanged("IsExpanded");
+                }
+
+                // Expand all the way up to the root.
+                if (_isExpanded && _parent != null)
+                    _parent.IsExpanded = true;
+            }
+        }
+
+        #endregion // IsExpanded
+
+        #region IsSelected
+
+        /// <summary>
+        /// Gets/sets whether the TreeViewItem 
+        /// associated with this object is selected.
+        /// </summary>
+        public bool IsSelected
+        {
+            get { return _isSelected; }
+            set
+            {
+                if (value != _isSelected)
+                {
+                    _isSelected = value;
+                    this.OnPropertyChanged("IsSelected");
+                }
+            }
+        }
+
+        #endregion // IsSelected
+
+        #region NameContainsText
+
+        public bool NameContainsText(string text)
+        {
+            if (String.IsNullOrEmpty(text) || String.IsNullOrEmpty(this.Name))
+                return false;
+
+            return this.Name.IndexOf(text, StringComparison.InvariantCultureIgnoreCase) > -1;
+        }
+
+        #endregion // NameContainsText
+
+        #region Parent
+
+        public PersonViewModel Parent
+        {
+            get { return _parent; }
+        }
+
+        #endregion // Parent
+
+        #endregion // Presentation Members
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion // INotifyPropertyChanged Members
+    }
+
+    /// <summary>
+    /// This is the view-model of the UI.  It provides a data source
+    /// for the TreeView (the FirstGeneration property), a bindable
+    /// SearchText property, and the SearchCommand to perform a search.
+    /// </summary>
+    public class FamilyTreeViewModel
+    {
+        #region Data
+
+        readonly ReadOnlyCollection<PersonViewModel> _firstGeneration;
+        readonly PersonViewModel _rootPerson;
+        readonly ICommand _searchCommand;
+
+        IEnumerator<PersonViewModel> _matchingPeopleEnumerator;
+        string _searchText = String.Empty;
+
+        #endregion // Data
+
+        #region Constructor
+
+        public FamilyTreeViewModel(Person rootPerson)
+        {
+            _rootPerson = new PersonViewModel(rootPerson);
+
+            _firstGeneration = new ReadOnlyCollection<PersonViewModel>(
+                new PersonViewModel[] 
+                { 
+                    _rootPerson 
+                });
+
+            _searchCommand = new SearchFamilyTreeCommand(this);
+        }
+
+        #endregion // Constructor
+
+        #region Properties
+
+        #region FirstGeneration
+
+        /// <summary>
+        /// Returns a read-only collection containing the first person 
+        /// in the family tree, to which the TreeView can bind.
+        /// </summary>
+        public ReadOnlyCollection<PersonViewModel> FirstGeneration
+        {
+            get { return _firstGeneration; }
+        }
+
+        #endregion // FirstGeneration
+
+        #region SearchCommand
+
+        /// <summary>
+        /// Returns the command used to execute a search in the family tree.
+        /// </summary>
+        public ICommand SearchCommand
+        {
+            get { return _searchCommand; }
+        }
+
+        private class SearchFamilyTreeCommand : ICommand
+        {
+            readonly FamilyTreeViewModel _familyTree;
+
+            public SearchFamilyTreeCommand(FamilyTreeViewModel familyTree)
+            {
+                _familyTree = familyTree;
+            }
+
+            public bool CanExecute(object parameter)
+            {
+                return true;
+            }
+
+            event EventHandler ICommand.CanExecuteChanged
+            {
+                // I intentionally left these empty because
+                // this command never raises the event, and
+                // not using the WeakEvent pattern here can
+                // cause memory leaks.  WeakEvent pattern is
+                // not simple to implement, so why bother.
+                add { }
+                remove { }
+            }
+
+            public void Execute(object parameter)
+            {
+                _familyTree.PerformSearch();
+            }
+        }
+
+        #endregion // SearchCommand
+
+        #region SearchText
+
+        /// <summary>
+        /// Gets/sets a fragment of the name to search for.
+        /// </summary>
+        public string SearchText
+        {
+            get { return _searchText; }
+            set
+            {
+                if (value == _searchText)
+                    return;
+
+                _searchText = value;
+
+                _matchingPeopleEnumerator = null;
+            }
+        }
+
+        #endregion // SearchText
+
+        #endregion // Properties
+
+        #region Search Logic
+
+        void PerformSearch()
+        {
+            if (_matchingPeopleEnumerator == null || !_matchingPeopleEnumerator.MoveNext())
+                this.VerifyMatchingPeopleEnumerator();
+
+            var person = _matchingPeopleEnumerator.Current;
+
+            if (person == null)
+                return;
+
+            // Ensure that this person is in view.
+            if (person.Parent != null)
+                person.Parent.IsExpanded = true;
+
+            person.IsSelected = true;
+        }
+
+        void VerifyMatchingPeopleEnumerator()
+        {
+            var matches = this.FindMatches(_searchText, _rootPerson);
+            _matchingPeopleEnumerator = matches.GetEnumerator();
+
+            if (!_matchingPeopleEnumerator.MoveNext())
+            {
+                MessageBox.Show(
+                    "No matching names were found.",
+                    "Try Again",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Information
+                    );
+            }
+        }
+
+        IEnumerable<PersonViewModel> FindMatches(string searchText, PersonViewModel person)
+        {
+            if (person.NameContainsText(searchText))
+                yield return person;
+
+            foreach (PersonViewModel child in person.Children)
+                foreach (PersonViewModel match in this.FindMatches(searchText, child))
+                    yield return match;
+        }
+
+        #endregion // Search Logic
+    }
+
     public class HierarchyViewModel : DockWindowViewModel
     {
+        public FamilyTreeViewModel FamilyTree { get; set; }
+
+        public HierarchyViewModel()
+        {
+            FamilyTree = new FamilyTreeViewModel(GetFamilyTree());
+        }
+
+        public static Person GetFamilyTree()
+        {
+            // In a real app this method would access a database.
+            return new Person
+            {
+                Name = "David Weatherbeam",
+                Children =
+                {
+                    new Person
+                    {
+                        Name="Alberto Weatherbeam",
+                        Children=
+                        {
+                            new Person
+                            {
+                                Name="Zena Hairmonger",
+                                Children=
+                                {
+                                    new Person
+                                    {
+                                        Name="Sarah Applifunk",
+                                    }
+                                }
+                            },
+                            new Person
+                            {
+                                Name="Jenny van Machoqueen",
+                                Children=
+                                {
+                                    new Person
+                                    {
+                                        Name="Nick van Machoqueen",
+                                    },
+                                    new Person
+                                    {
+                                        Name="Matilda Porcupinicus",
+                                    },
+                                    new Person
+                                    {
+                                        Name="Bronco van Machoqueen",
+                                    }
+                                }
+                            }
+                        }
+                    },
+                    new Person
+                    {
+                        Name="Komrade Winkleford",
+                        Children=
+                        {
+                            new Person
+                            {
+                                Name="Maurice Winkleford",
+                                Children=
+                                {
+                                    new Person
+                                    {
+                                        Name="Divinity W. Llamafoot",
+                                    }
+                                }
+                            },
+                            new Person
+                            {
+                                Name="Komrade Winkleford, Jr.",
+                                Children=
+                                {
+                                    new Person
+                                    {
+                                        Name="Saratoga Z. Crankentoe",
+                                    },
+                                    new Person
+                                    {
+                                        Name="Excaliber Winkleford",
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+        }
     }
 }
